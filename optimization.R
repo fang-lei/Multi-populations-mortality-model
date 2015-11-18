58affe46e0881504aee380dea358d6cde5fee6a5
Lei Fang <lei.fang@outlook.de>
2015-11-18 16:49:38 +0100

    20151118-05

--- optimization.R
+++ optimization.R
@@ -9,18 +9,13 @@ optimization = function (theta, kt, kt.reference) {
       dref = data.frame (kt.reference, t.reference)
       sm.t = (t - theta2) /theta3 # time adjustment
       sm = locpol (kt.reference~t.reference, dref, kernel = EpaK, xeval = sm.t) # time-adjusted kt based on smoothed reference curve
-      mu = theta1 * sm$lpFit[,2] + theta4 # modelled kt based on initial theta
+      mu = theta1 * sm$lpFit[,2] + theta4 # modelled kt based on theta
       mse = mean ((kt-mu)^2) # mse of kt and the modelled one
       return (mse)
     }
     out = optim (theta, loss, gr = NULL, t, kt, t.reference, kt.reference, control = list (maxit = 1000))
+    loss.mse = out$value
     optimal.theta = out$par
-    dref = data.frame (kt, t)
-    sm.t1 = (t - optimal.theta[2]) /optimal.theta[2] # time adjustment
-    sm1 = locpol (kt~t, dref, kernel = EpaK, xeval = sm.t1) # time-adjusted kt based on smoothed reference curve
-    shift.kt1 = optimal.theta[1] * sm1$lpFit[,2] + optimal.theta[4] # modelled kt based on initial theta
-    shift.kt = ts (shift.kt1, start = sm.t1[1], frequency = 1)
     error.theta = mean ((theta - optimal.theta)^2)
-    error.kt = mean ((kt - shift.kt)^2)
-return ( list( optimal.theta, shift.kt, error.theta, error.kt ))
+return ( list( loss.mse, optimal.theta, error.theta ))
 }
