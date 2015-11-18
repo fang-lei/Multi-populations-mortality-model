480d644316571ac7609729dc076aceec703ca827
Lei Fang <lei.fang@outlook.de>
2015-11-18 11:10:56 +0100

    20151118-01

--- data.R
+++ data.R
@@ -4,7 +4,7 @@
 # ------------------------------------------------------------------------------
 # Quantlet:    data.R
 # ------------------------------------------------------------------------------
-# Description: Read data from Human Mortality Database.
+# Description: Read data from Human Mortality Database and calculate kt.
 # ------------------------------------------------------------------------------
 # Keywords:    Human Mortality Database, mortality, Lee-Carter method,
 #              multi-populations
@@ -35,6 +35,7 @@ China.mort.female = demogdata (data3, data3pop, ages.mort, years.mort, type = 'm
 China.mort.male.adjust = smooth.demogdata (China.mort.male, b = 65, k = 30)
 China.mort.female.adjust = smooth.demogdata (China.mort.female, b = 65, k = 30)
 
+# estimate kt of China
 China.lca.female = lca (China.mort.female.adjust, series = "female", adjust = "dt", max.age = 90,interpolate = TRUE)
 ax.China.female = China.lca.female$ax
 bx.China.female = China.lca.female$bx
@@ -60,15 +61,16 @@ names = c ("Australia","Austria","Belarus","Bulgaria","Canada","Chile","CzechRep
 loop1 = length(names)
 for (i in 1:(loop1 - 1)) {
   nam1 = paste (names [i] )
-  assign (nam1, hmd.mx (shortnames[i], "fanglei@hu-berlin.de", "1440177160", names[i]))
-  temp1 = hmd.mx (shortnames[i], "fanglei@hu-berlin.de", "1440177160", names[i])
+  assign (nam1, hmd.mx (shortnames[i], "fanglei@hu-berlin.de", "1440177160", names[i])) # read data from HMD
+  temp1 = hmd.mx (shortnames[i], "fanglei@hu-berlin.de", "1440177160", names[i]) # accound and password
   nam2 = paste (names[i], "lca.female", sep = ".")
-  assign (nam2, lca (temp1, series = "female", adjust = "dt", interpolate = TRUE))
+  assign (nam2, lca (temp1, series = "female", adjust = "dt", interpolate = TRUE)) # Lee-Carter method
   temp2 = lca (temp1, series = "female", adjust = "dt", interpolate = TRUE)
-  nam3 = paste ("ax", names[i], "female", sep = ".")
+  nam3 = paste ("ax", names[i], "female", sep = ".") # ax
   assign (nam3, temp2$ax)
-  nam4 = paste ("bx", names[i], "female", sep = ".")
+  nam4 = paste ("bx", names[i], "female", sep = ".") # bx
   assign (nam4, temp2$bx)
-  nam5 = paste ("kt", names[i], "female", sep = ".")
+  nam5 = paste ("kt", names[i], "female", sep = ".") # kt
   assign (nam5, temp2$kt)
 }
+
