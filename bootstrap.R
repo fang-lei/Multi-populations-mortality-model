780c8d959f642ec9339eb282987f0ac82da76b53
Lei Fang <lei.fang@outlook.de>
2016-04-27 20:39:33 +0200

    20160427-3
    with loss function highlighted

--- bootstrap.R
+++ bootstrap.R
@@ -80,3 +80,38 @@ boot.2 = tsboot (boot.data, test.func, R = 50, l = 12, sim = "fixed")
 boot.2$t
 boot.2$t0
 boot.ci(boot.2, index=1) 
+
+
+
+### loss function
+loss = function (theta, t, kt, t.reference, kt.reference) {
+  ## assume theta[1]>0, theta[3]>0, if not, take absolute values
+  theta1 = abs(theta[1])
+  theta2 = theta[2]
+  theta3 = abs(theta[3])
+  
+  if (theta[1]<0 | theta[3]<0) warning("theta1 or theta3 <0, abs value is used")
+  sm.t = (t.reference - theta2) /theta3 # time adjustment
+  ## common domain for kt and time-adjusted kt.reference
+  tmin=max(min(t), min(sm.t))
+  tmax=min(max(t), max(sm.t)) 
+  i0=which(t>=tmin & t<=tmax) # index for common domain
+  if (length(i0)>0){
+    t0=t[i0]  
+    ## smooth interpolation of shifted kt.reference on common grid    
+    dref=data.frame(sm.t=sm.t, kt.reference=kt.reference)
+    
+    # way 3 - sm: sm.regression with optimal smoothing parameter
+    #       h.optimal3 = h.select(sm.t, kt.reference)
+    #       sm = sm.regression(sm.t, kt.reference, h = h.optimal3, eval.points = t0, model = "none", poly.index = 1, display="none")
+    #       mu = theta1 * sm$estimate
+    
+    # way 5 - stats: ksmooth
+    sm = ksmooth (sm.t, kt.reference, kernel = "normal", bandwidth = bw.default, n.points = length(t0), range.x = range(t0))
+    mu = theta1 * sm$y
+    
+    ## mean squared error at common grid points
+    mse = mean ((kt[i0]-mu)^2) # mse of kt and the modelled one
+  } else { mse=1e9 }
+  return (mse)
+}
